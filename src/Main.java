b14c141a6ffe505a26133d20084f0948c5687f7f
Kristin Brooks <kristin.ruth.brooks@gmail.com>
2019-11-02 15:28:19 -0700

    starting files for project 1

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+//*********************************************************************************************************************
+// CLASS: Main
+//
+// DESCRIPTION
+//
+//
+// COURSE AND PROJECT INFO
+// CSE205 Object Oriented Programming and Data Structures, Fall B 2019
+// Project Number: 1
+//
+// AUTHOR: Kristin Brooks, krbrook7, krbrook7@asu.edu
+//*********************************************************************************************************************
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.PrintWriter;
+
+public class Main {
+    public static void main(String[] args) {
+        new Main().run();
+    }
+    /**
+     *
+     */
+    private void run() {
+        ArrayList<Integer> list = new ArrayList<>();
+
+    }
+}
